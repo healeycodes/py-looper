69945c2e6e9dae5b20919ba9c59a7bc42f42c013
Andrew <healeycodes@gmail.com>
2018-07-29 03:02:25 +0100

    Fix min length and double length issues

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ try:
     audio_src = sys.argv[2]
     min_length = int(str(sys.argv[3]).split(  # format: Minutes.Seconds
         '.')[0]) * 60 + int(str(sys.argv[3]).split('.')[1])  # converted to seconds
+
+    if min_length < 1:
+        min_length = 1  # keep min_length no smaller than one second
+
     movie_name = sys.argv[4] + '.mp4'
 except:
     print('Error: Wrong or missing command line arguments!')
-    print('Example args: my_image.png my_audio.mp3 1.0 my_movie')
+    print('Example args: my_image.png my_audio.mp3 0.30 my_movie')
     print('[image] [audio] [min_length] [movie_name]')
+    print('min_length is formatted as Minutes.Seconds')
     input('All args are required. Quiting..')
     sys.exit(1)
 
@@ -30,7 +35,7 @@ audio = audio_loop.audio_loop(audio, nloops=loops_needed)
 image_clip = image_clip.set_audio(audio)
 image_clip = image_clip.resize(width=1080)
 image_clip.fps = 24
-image_clip.duration = (audio.duration * loops_needed) - audio.duration
+image_clip.duration = audio.duration
 
 # render!
 image_clip.write_videofile(movie_name, preset='ultrafast',
