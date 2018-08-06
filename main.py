8ab0f0fb7170ba1c0dde134daa2acab7555d35ba
Andrew <healeycodes@gmail.com>
2018-08-06 19:38:00 +0100

    Add detailed error message for sys arguments

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import sys
+import os
 import multiprocessing
 from moviepy.editor import ImageClip, AudioFileClip
 from moviepy.audio.fx import audio_loop
 
-# command line arguments [0, image, audio, min_length, movie_name]
+
+def error(err):
+    print('Command line argument error.\n')
+    print('Error: {}\n'.format(err))
+    print('Required args: [image] [audio] [min_length] [movie_name]')
+    print('E.g., my_image.png my_audio.mp3 0.30 my_movie')
+    print('Note: [min_length] is formatted as Minutes.Seconds')
+    input('Quiting..')
+    sys.exit(1)
+
+
+# command line arguments [main.py, image, audio, min_length, movie_name]
+if len(sys.argv) < 5:
+    error('{0} args supplied but {1} args required.'.format(len(sys.argv) - 1, 4))
+
 try:
     image_src = sys.argv[1]
     audio_src = sys.argv[2]
+
     min_length = int(str(sys.argv[3]).split(  # format: Minutes.Seconds
         '.')[0]) * 60 + int(str(sys.argv[3]).split('.')[1])  # converted to seconds
-
     if min_length < 1:
         min_length = 1  # keep min_length no smaller than one second
 
     movie_name = sys.argv[4] + '.mp4'
 except:
-    print('Error: Wrong or missing command line arguments!')
-    print('[image] [audio] [min_length] [movie_name]')
-    print('Example args: my_image.png my_audio.mp3 0.30 my_movie')
-    print('[min_length] is formatted as Minutes.Seconds')
-    input('All args are required. Quiting..')
-    sys.exit(1)
+    error('Couldn\'t retrieve files')
+
 
 image_clip = ImageClip(image_src)  # get image
 audio = AudioFileClip(audio_src)  # get audio
